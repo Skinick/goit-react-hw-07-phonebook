b16a5da306c48b39cd656087356db925da759dec
Skinick <107771280+Skinick@users.noreply.github.com>
2023-08-29 00:53:31 +0300

    remove isDublicate from contacts-operations

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -14,50 +14,26 @@ export const fetchContacts = createAsyncThunk(
   }
 );
 
-const isDublicate = ({ name, number }, contacts) => {
-    const normalizedName = name.toLowerCase();
-    const normalizedNumber = number.toLowerCase();
-  
-    const result = contacts.find(item => {
-      return (
-        normalizedName === item.name.toLowerCase() ||
-        normalizedNumber === item.number.toLowerCase()
-      );
-    });
-  
-    return Boolean(result);
-  };
-  
-  export const addContact = createAsyncThunk(
-    'contacts/add',
-    async (data, { rejectWithValue }) => {
-      try {
-        const result = await api.addContact(data);
-        return result;
-      } catch (error) {
-        return rejectWithValue(error);
-      }
-    },
-    {
-      condition: (data, { getState }) => {
-        const { contacts } = getState();
-        if (isDublicate(data, contacts.items)) {
-          alert(`${data.name} or ${data.number} is alredy exist`);
-          return false;
-        }
-      },
+export const addContact = createAsyncThunk(
+  'contacts/add',
+  async (data, { rejectWithValue }) => {
+    try {
+      const result = await api.addContact(data);
+      return result;
+    } catch (error) {
+      return rejectWithValue(error);
     }
-  );
-  
-  export const removeContact = createAsyncThunk(
-    'contacts/remove',
-    async (id, { rejectWithValue }) => {
-      try {
-        await api.removeContact(id);
-        return id;
-      } catch (error) {
-        return rejectWithValue(error);
-      }
+  }
+);
+
+export const removeContact = createAsyncThunk(
+  'contacts/remove',
+  async (id, { rejectWithValue }) => {
+    try {
+      await api.removeContact(id);
+      return id;
+    } catch (error) {
+      return rejectWithValue(error);
     }
-  );
-  
+  }
+);
